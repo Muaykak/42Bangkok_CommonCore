ff6fc1faf9d122a6fdfc71dd45e2caf087f1af91
muaykak <sutawith001@gmail.com>
2024-09-11 16:40:47 +0000

    .

--- Libft_1/Libft/libft.h
+++ Libft_1/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: srussame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 13:56:55 by srussame          #+#    #+#             */
-/*   Updated: 2024/08/25 13:56:58 by srussame         ###   ########.fr       */
+/*   Updated: 2024/09/11 15:54:27 by srussame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,12 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_putchar_fd(char c, int fd);
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
+//t_list	*ft_lstnew(void *content);
+//void	ft_lstadd_front(t_list **lst, t_list *new);
+//int		ft_lstsize(t_list *lst);
+//t_list	*ft_lstlast(t_list *lst);
+//void	ft_lstadd_back(t_list **lst, t_list *new);
+//void	ft_lstdelone(t_list *lst, void (*del)(void *));
+//void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
