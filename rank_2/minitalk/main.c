2e45f3ea6b5808cd67d17a29bfa4015d2c212a8f
muaykak <sutawith001@gmail.com>
2024-11-09 05:51:39 +0000

    ,

--- rank_2/minitalk/main.c
+++ rank_2/minitalk/main.c
@@ -6,7 +6,7 @@
 /*   By: muaykak <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 05:54:37 by muaykak           #+#    #+#             */
-/*   Updated: 2024/11/08 05:57:19 by muaykak          ###   ########.fr       */
+/*   Updated: 2024/11/09 05:49:43 by muaykak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,12 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void sighandler(int);
-
-int main () {
-   signal(SIGINT, sighandler);
-
-   while(1) {
-      ft_printf("Going to sleep for a second...\n");
-      sleep(1); 
-   }
-   return(0);
+int main(void)
+{
+        int ret = 0;
+        ret = pause();
+        ft_printf("Pause returned with %d\n", ret);
+        return 0;
 }
 
-void sighandler(int signum) {
-   ft_printf("Caught signal %d, coming out...\n", signum);
-   exit(1);
-}
+
