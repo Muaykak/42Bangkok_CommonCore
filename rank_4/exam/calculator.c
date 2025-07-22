64f4803635a89cc0203f8357c9ccdebf982c61c5
srussame <sutawih@gmail.com>
2025-07-22 15:55:56 +0000

    .

--- rank_4/exam/calculator.c
+++ rank_4/exam/calculator.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+typedef enum e_type {
+	VAL,
+	ADD,
+	MULTI
+}			t_type;
+
 typedef struct s_cal_node {
-	enum e_type {
-		VAL,
-		ADD,
-		MULTI
-	}					type;
+
+	t_type				type;
 	int					value;
 	struct s_cal_node	*left;
 	struct s_cal_node	*right;
@@ -61,6 +66,58 @@ t_cal_node *parsing(char *argv)
 {
 
 }
+/*
+
+EXAM RANK 4
+						   104
+			<<- "(((((2 x 4) + 1) + 7) + 2 x 2) x 5 + 4)"
+
+							+
+						   / \
+						  x   4
+						 / \
+						+   5
+					  /   \
+					 +     x
+				   /  \   /  \
+				  +    7  2   2 
+				/   \
+			    x	1
+			  /  \
+			 2    4
+*/
+t_cal_node	*fake_parsing(void)
+{
+	return (new_node(node_info(ADD, 0, new_node(node_info(MULTI, 0, \
+new_node(node_info(ADD, 0, new_node(node_info(ADD, 0, \
+new_node(node_info(ADD, 0, new_node(node_info(MULTI, 0, \
+new_node(node_info(VAL, 2, NULL, NULL)), \
+new_node(node_info(VAL, 4, NULL, NULL)))), \
+new_node(node_info(VAL, 1, NULL, NULL)))), \
+new_node(node_info(VAL, 7, NULL, NULL)))), \
+new_node(node_info(MULTI, 0, new_node(node_info(VAL, 2, NULL, NULL)), \
+new_node(node_info(VAL, 2, NULL, NULL)))))), \
+new_node(node_info(VAL, 5, NULL, NULL)))), new_node(node_info(VAL, 4, NULL, NULL)))));
+}
+
+/*
+	Example: 2+2+2
+
+		+
+       / \
+	  2   +
+	  	 / \
+		2   2
+	
+	Example: 1x2+3   				1x(2+3)
+
+				+						x
+			   / \					   / \
+			  x   3					  1   +
+			 / \						 / \
+			1   2						2   3
+
+*/
 
 /*
 This calculation program doesn't need to handle subtraction and division
@@ -77,7 +134,7 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		return (1);
 	
-	cal_node = parsing(argv[1]);
+	cal_node = fake_parsing();
 	printf("result = %d\n", calculation(cal_node));
 	destroy_node(cal_node);
 	return (0);
