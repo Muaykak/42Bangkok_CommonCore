6c9de9749ff84a3aea510621301c3afb77e24252
muaykak <sutawith001@gmail.com>
2025-09-09 23:35:49 +0700

    .

--- rank_4/cpp01/ex04/main.cpp
+++ rank_4/cpp01/ex04/main.cpp
@@ -6,12 +6,11 @@
 /*   By: srussame <sutawith@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 16:11:54 by srussame          #+#    #+#             */
-/*   Updated: 2025/09/09 18:19:00 by srussame         ###   ########.fr       */
+/*   Updated: 2025/09/09 23:33:45 by srussame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
+#include "replace.hpp"
 
 int	main(int argc, char **argv)
 {
@@ -22,11 +21,22 @@ int	main(int argc, char **argv)
 	}
 
 	std::ifstream my_file(argv[1]);
-	if (my_file.good() == false)
+
+	if (my_file.is_open() == false)
 	{
 		std::cerr << "The file cannot open" << std::endl;
 		return (1);
 	}
-	my_file.close();
-	(void)argv;
+
+	std::ofstream outfile;
+
+	outfile.open(OUTFILE_NAME);
+	if (outfile.is_open() == false)
+	{
+		std::cerr << "The outfile cannot open" << std::endl;
+		return (1);
+	}
+	if (replace(my_file, outfile, argv[2], argv[3]) == false)
+		return (1);
+	return (0);
 }
